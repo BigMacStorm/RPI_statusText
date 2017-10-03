838b880c310b8c4b07f68123693f3cf7dbcd7394
Aaron Whitehead <aaronbmwhitehead@gmail.com>
2017-10-03 05:45:43 +0000

    updated pin mappings, supports lcd out now, ready to actually build the software. Might even attach pictures later.

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
+import Adafruit_CharLCD as LCD
+
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+lcd_rs = 25
+lcd_en = 24
+lcd_d4 = 23
+lcd_d5 = 17
+lcd_d6 = 21
+lcd_d7 = 22
+lcd_bl = 4
+
+cols = 16
+rows = 2
+
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, cols, rows, lcd_bl)
+
+lcd.message('Hello\nWorld!')
+
 
 count = 0
 lastVal = True
@@ -12,6 +29,7 @@ while(True):
 		lastVal = inputVal
 		if(inputVal):
 			count += 1
-			print count
+			lcd.clear()
+			lcd.message(str(count))
 	time.sleep(0.01)
 
